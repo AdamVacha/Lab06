d82bb3a6f4ce44f0f37707ee6f6ba6cfc8e3e6e5
Adam Vacha <adamrvacha@gmail.com>
2022-10-31 14:41:45 -0400

    added substract feature

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 def add(a, b):
     return a + b
 
+
+def subtract(a, b):
+    return a - b
+
+
 # please list more examples of how add works
-if __name__ == "__main__"
+if __name__ == "__main__":
     print(add(1, 4))
+    print(subtract(2, 7))
